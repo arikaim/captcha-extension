bcf28b87a5d2b7a85ad62ce45525e7df263bf036
arikaim <info@arikaim.com>
2025-01-30 12:02:53 +0200

    =

--- view/components/admin/view/view.js
+++ view/components/admin/view/view.js
@@ -10,8 +10,7 @@ function CaptchaControlPanelView() {
     var self = this;
 
     this.init = function() {
-        arikaim.events.on('driver.config',function(element,name,category) {
-            arikaim.ui.setActiveTab('#settings_button');
+        arikaim.events.on('driver.config',function(element,name,category) {         
             return drivers.loadConfig(name,'capcha_driver_config');           
         },'driverConfig');       
         
--- view/components/code/cloudflare/cloudflare.js
+++ view/components/code/cloudflare/cloudflare.js
@@ -1,9 +1,1 @@
 'use strict';
-
-window.onloadTurnstileCallback = function () {
-    var siteKey = $('#turnsite_captcha').attr('data-sitekey');
-
-    turnstile.render('#turnsite_captcha',{
-        sitekey: siteKey
-    });
-};
